769227d77875ff5af4929d47db165669c5d33b08
Ernestocanada <ernesto.industrial@gmail.com>
2021-10-11 17:20:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const port = 80;
+let users = [];
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+const addUser = (userName, roomId) => {
+  users.push({
+    userName: userName,
+    roomId: roomId,
+  });
+};
+
+const userLeave = (userName) => {
+  users = users.filter((user) => user.userName !== userName);
+};
+
+const getRoomUsers = (roomId) => {
+  return users.filter((user) => user.roomId === roomId);
+};
+
+io.on("connection", (socket) => {
+  console.log("Someone Connected");
+  socket.on("join-room", ({ roomId, userName }) => {
+    console.log("User Joined room");
+    console.log(roomId);
+    console.log(userName);
+    socket.join(roomId);
+    addUser(userName, roomId);
+    socket.to(roomId).emit("user-connected", userName);
+    io.to(roomId).emit("all-users", getRoomUsers(roomId));
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+      socket.leave(roomId);
+      userLeave(userName);
+      io.to(roomId).emit("all-users", getRoomUsers(roomId));
+    });
+  });
+});
+server.listen(port, () => {
+  console.log(`app is running on localhost:${port}`);
+});
